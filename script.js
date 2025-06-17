cfcc40ed8bbbb800300cab02eb7021a4285516ba
Mlungisi27 <mlungisisotiya7@gmail.com>
2025-06-17 15:02:11 +0200

    Enhance contact retrieval and display functionality with error handling and loading states

--- script.js
+++ script.js
@@ -68,3 +68,56 @@ function showEditContacts(contactId) {
     // Load contact data for editing
     loadContactForEdit(contactId);
 }
+
+function getContacts() {
+    const contactsList = document.getElementById('contactsList');
+    contactsList.innerHTML = '<div class="loading"> Loading contacts...</div>';
+
+    fetch(rootPath + "controller/get-contacts/")
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (data){
+            displayContacts(data);
+        })
+        .catch(function (error) {
+            contactsList.innerHTML = '<div class="error">Error loading contacts. Please try again .</div>'
+        });  
+}
+
+function displayContacts(contacts) {
+    const contactsList = document.getElementById('contactsList');
+
+    if (!contacts || contacts.length ===0) {
+        contactsList.innerHTML = '<div class="loading">No contacts found.Add a new contact.</div>';
+        return;
+    }
+
+    let html = '<div class="contacts-grid">';
+
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+
+        let avatarSrc = contact.avatar ? 
+            `${rootPath}controller/uploads/${contact.avatar}` :
+            `https://vi-avatars.com/api/?name=${contact.firstname}+${contact.lastname}&background=random&size=100`;
+
+            html += `
+                    <div class="contact-card>
+                        <img src="${avatarSrc}" alt="${contact-avatar}">
+                        <div class="contact-name">${contact.firstname} ${contact.lastname}</div>
+                        <div class="contact-details">
+                            <p><strong>📲 Mobile:</strong> ${contact.mobile}</p>
+                            <p><strong>📧 Email:</strong> ${contact.email}</p>
+                        </div>
+                        <div class="contact-actions">
+                            <button class="btn btn-secondary" onclick="showEditContacts('${contact.id}')">&#9999 Edit</button>
+                            <button class="btn btn-danger" onclick="deleteContact('${contact.id}')">&#10060 Delete</button>
+                        </div>
+                    </div>
+                    `;
+    }
+
+    html += '</div>';
+    contactsList.innerHTML = html;
+}
